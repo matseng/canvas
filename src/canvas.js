ead35d4cc441805cccb96ee1a97916d48a39e611
Michael Tseng <mtseng@MTSE-C02JG0Z2DRVG.local>
2014-11-05 18:12:02 -0800

    Added 2 objects. Improved removal of mouse listeners

--- src/canvas.js
+++ src/canvas.js
@@ -17,9 +17,11 @@ Rect.prototype.getDims = function() {
 
 var CanvasDemo = function() {
   this.canvas = document.getElementById('canvas');
-  this.rect = new Rect(25,25,100,100);
+  this.shapes = [];
 
   if ( this.canvas.getContext ) {
+    this.shapes.push( new Rect(25,25,100,100) );
+    this.shapes.push( new Rect(125,125,200,200) );
     this.ctx = canvas.getContext('2d');
     this.resizeCanvas();
     window.onresize = this.resizeCanvas.bind(this);
@@ -30,9 +32,9 @@ var CanvasDemo = function() {
 
 var mousePointInitial = {};
 var shapePointInitial = {};
-var _reset;
 var mousemoveBound;
 var mouseupBound;
+var _resetBound;
 
 CanvasDemo.prototype.addEventListeners = function() {
   this.canvas.addEventListener('mousedown', this.mousedown.bind(this));
@@ -40,36 +42,42 @@ CanvasDemo.prototype.addEventListeners = function() {
 
 CanvasDemo.prototype.mousedown = function(event) {
   var point = this.canvas.relMouseCoords(event);
-  if ( this.inBounds(point) ) {
+  var shape = this.getShapeinBounds(point);
+  if ( shape ) {
     mousePointInitial = point;
-    shapePointInitial = {x: this.rect.x, y: this.rect.y};
-    var mousemoveBound = this.mousemove.bind(this);
-    var mouseupBound = this.mouseup.bind(this);
+    shapePointInitial = {x: shape.x, y: shape.y};
+    mousemoveBound = this.mousemove.bind(this, shape);
+    mouseupBound = this.mouseup.bind(this);
+    _resetBound = _reset.bind(this, mousemoveBound, mouseupBound);
     this.canvas.addEventListener('mousemove', mousemoveBound);
     this.canvas.addEventListener('mouseup', mouseupBound);
-    _reset.bind(null, mousemoveBound, mouseupBound);
+  } else {
+    _resetBound.call(this);
   }
 };
 
-CanvasDemo.prototype.mousemove = function(event) {
+CanvasDemo.prototype.mousemove = function(shape, event) {
   if (event.which === 1 && mousePointInitial) {
-    this.ctx.clearRect.apply(this.ctx, this.rect.getDims());
+    this.ctx.clearRect.apply(this.ctx, shape.getDims());
     var mousePoint = this.canvas.relMouseCoords(event);
     var deltaX = mousePoint.x - mousePointInitial.x;
     var deltaY = mousePoint.y - mousePointInitial.y;
-    this.rect.x = shapePointInitial.x + deltaX;
-    this.rect.y = shapePointInitial.y + deltaY;
-    this.ctx.fillRect.apply(this.ctx, this.rect.getDims());
+    shape.x = shapePointInitial.x + deltaX;
+    shape.y = shapePointInitial.y + deltaY;
+    this.ctx.fillRect.apply(this.ctx, shape.getDims());
   } else {
-    _reset();
+    if (_resetBound) {
+      debugger     
+      _resetBound();
+    }
   }
 }
 
 CanvasDemo.prototype.mouseup = function(event) {
-  _reset();
+  _resetBound();
 }
 
-function _reset (mousemoveBound, mouseupBound) {
+function _reset(mousemoveBound, mouseupBound) {
   mousePointInitial = null;
   shapePointInitial = null;
   this.canvas.removeEventListener('mousemove', mousemoveBound);
@@ -78,25 +86,24 @@ function _reset (mousemoveBound, mouseupBound) {
 
 
 CanvasDemo.prototype.draw = function() {
-  
-  this.ctx.fillStyle = 'rgb(200,0,0)';
-  // this.ctx.fillRect(10, 10, 100, 50);
-  this.ctx.fillRect.apply(this.ctx, this.rect.getDims());
-  this.ctx.clearRect(50,50,50,50);
-  this.ctx.strokeRect(45,45,60,60);
-
-  // this.ctx.fillStyle = 'rgba(0,0,200,0.5)';
-  // this.ctx.fillRect(30, 30, 100, 50);
-
-
-  // this.ctx.fillRect(25,25,100,100);
-  // this.ctx.clearRect(45,45,60,60);
-  // this.ctx.strokeRect(50,50,50,50);
+  for(var i = 0; i < this.shapes.length; i++) {
+    var shape = this.shapes[i];
+    this.ctx.fillStyle = 'rgba(200,0,0,0.5)';
+    this.ctx.fillRect.apply(this.ctx, shape.getDims());
+    this.ctx.clearRect(50,50,50,50);
+    this.ctx.strokeRect(45,45,60,60);
+  }
 };
 
-CanvasDemo.prototype.inBounds = function(point) {
+CanvasDemo.prototype.getShapeinBounds = function(point) {
   var shape = this.rect;
-  return ( (shape.x <= point.x) && (point.x <= shape.x + shape.width) && (shape.y <= point.y) && (point.y <= shape.y + shape.height) );
+  for(var i = 0; i < this.shapes.length; i++) {
+    shape = this.shapes[i];
+    if ( (shape.x <= point.x) && (point.x <= shape.x + shape.width) && (shape.y <= point.y) && (point.y <= shape.y + shape.height) ){
+      return shape;
+    }
+  }
+  return null;
 };
 
 CanvasDemo.prototype.resizeCanvas = function() {
