898228e8c72faf710d98fc956842e939f71c6b71
Michael Tseng <matseng@gmail.com>
2014-11-07 19:39:12 -0800

    Added firebase method in collection module - able to load note shapes

--- src/collection/collection.js
+++ src/collection/collection.js
@@ -10,6 +10,21 @@ module.exports = (function() {
   Collection.prototype.run = function() {
     this.add( new Rect(25,25,100,100) );
     this.add( new Rect(125,125,200,200) );
+    this.firebase();
+  };
+
+  Collection.prototype.firebase = function() {
+    var ref = new Firebase('https://brainspace-biz.firebaseio.com/');
+    var notesRef = ref.child('notes2');
+
+    var i = 0;
+    notesRef.on("child_added", function(snapshot) {
+      var note = snapshot.val();
+      console.log(i++, note.style);
+      this.add( new Rect(note.data.x, note.data.y, note.style.width, note.style.height));
+      // this.add( new Rect(note.style.left, note.style.top, note.style.width, note.style.height));
+    }.bind(this));
+
   };
 
   Collection.prototype.add = function(shape) {
