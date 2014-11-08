381a409e751dd381e03af446ede4ffea97bccc86
Michael Tseng <matseng@gmail.com>
2014-11-08 11:02:25 -0800

    Fixed another typo with ghost click

--- src/collection/collection.js
+++ src/collection/collection.js
@@ -20,7 +20,6 @@ module.exports = (function() {
     var i = 0;
     notesRef.on("child_added", function(snapshot) {
       var note = snapshot.val();
-      console.log(i++, note.style);
       this.add( new Rect(note.data.x, note.data.y, note.style.width, note.style.height));
       // this.add( new Rect(note.style.left, note.style.top, note.style.width, note.style.height));
     }.bind(this));
