63be5c72f079e8c5b783165228a0ae3f662bef2e
Michael Tseng <matseng@gmail.com>
2014-11-08 16:29:55 -0800

    Added getNoteInBounds to eventually replace shape

--- src/collection/collection.js
+++ src/collection/collection.js
@@ -3,6 +3,7 @@ var Rect = require('./shapes.js')
 module.exports = (function() {
 
   var Collection = function() {
+    this.notes =[];
     this.shapes = [];
     this.run();
   }
@@ -16,10 +17,9 @@ module.exports = (function() {
   Collection.prototype.firebase = function() {
     var ref = new Firebase('https://brainspace-biz.firebaseio.com/');
     var notesRef = ref.child('notes2');
-
-    var i = 0;
     notesRef.on("child_added", function(snapshot) {
       var note = snapshot.val();
+      this.notes.push(note);
       this.add( new Rect(note.data.x, note.data.y, note.style.width, note.style.height));
       // this.add( new Rect(note.style.left, note.style.top, note.style.width, note.style.height));
     }.bind(this));
@@ -32,6 +32,22 @@ module.exports = (function() {
 
   Collection.prototype.get = function() {
     return this.shapes;
+  };
+
+  Collection.prototype.getNoteInBounds = function(point) {
+    var note;
+    for(var i = 0; i < this.notes.length; i++) {
+      note = this.notes[i];
+      if ( 
+        note.data.x <= point.x && 
+        point.x <= note.data.x + note.style.width && 
+        note.data.y <= point.y && 
+        point.y <= note.data.y + note.style.height 
+      ) {
+        return note;
+      }
+    }
+    return null;
   };
   
   return new Collection();
